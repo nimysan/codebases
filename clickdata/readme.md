44694b1ca94845307dac1699fb47ded80128f14d
yexw <yexw@amazon.com>
2022-12-24 15:54:44 +0800

    add modules

---
+++ clickdata/readme.md
@@ -0,0 +1,8 @@
+# IDEA
+
+## IDEA Spring依赖包找不到
+
+```bash
+#更新idea配置文件
+mvn -U idea:idea
+```
---
+++ clickdata/src/main/java/top/cuteworld/datagen/clickdata/config/KafkaConfig.java
@@ -0,0 +1,53 @@
+package top.cuteworld.datagen.clickdata.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+import top.cuteworld.datagen.clickdata.model.UserActionEmitter;
+import top.cuteworld.datagen.clickdata.model.UserBehaviorItem;
+
+import java.util.function.Function;
+
+@Slf4j
+@Configuration
+public class KafkaConfig {
+
+    private final String TOPIC_NAME = "flink-user-behavior-data";
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(TOPIC_NAME)
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+
+    @Bean
+    public UserActionEmitter emitter(KafkaTemplate<String, String> kafkaTemplate) {
+        UserActionEmitter userActionEmitter = new UserActionEmitter(new Function<UserBehaviorItem, Void>() {
+            @Override
+            public Void apply(UserBehaviorItem userBehaviorItem) {
+                kafkaTemplate.send(TOPIC_NAME, toJson(userBehaviorItem));
+                return null;
+            }
+
+            private String toJson(UserBehaviorItem userBehaviorItem) {
+                return "123";
+            }
+        });
+        userActionEmitter.batchEmit();
+        return userActionEmitter;
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send(TOPIC_NAME, "test");
+        };
+    }
+}
